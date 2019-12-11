3f9a6abef44dc8d34e961ad41e518b151b907bab
tmdalsvt <54830688+tmdalsvt@users.noreply.github.com>
2019-12-11 20:13:49 +0900

    Add files via upload

---
+++ src/Component/Profile/Profile.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Profile.css';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className='Profile' id='profile'>
+        <div className='Profile_Title'>
+          <h1 className="Main_Title">Profile</h1>
+        </div>
+        <div className="Profile_Content">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
