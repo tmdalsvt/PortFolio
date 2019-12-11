d79350c34490b43aa2ea6e667bacbbdc7bf4927c
tmdalsvt <54830688+tmdalsvt@users.noreply.github.com>
2019-12-11 20:16:30 +0900

    Add files via upload

---
+++ src/Component/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Contact.css';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className='Contact' id='contact'>
+        <div className='Contact_Title'>
+          <h1 className="Main_Title">Contact</h1>
+        </div>
+        <div className="Contact_Content">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
