19c1b1c5bd63b16f3544b1cf4ca256b568ba2c78
tmdalsvt <54830688+tmdalsvt@users.noreply.github.com>
2019-12-11 20:13:12 +0900

    Add files via upload

---
+++ src/Component/Menu/Menu.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Navbar} from '../../../node_modules/react-bootstrap';
+
+class Menu extends Component {
+  render() {
+    return (
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar.Brand>PortFolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link  onClick = {() => {
+              document.getElementById('main').scrollIntoView({behavior: "smooth"});
+            }}>Main</Nav.Link>
+            <Nav.Link onClick = {() => {
+              document.getElementById('profile').scrollIntoView({behavior: "smooth"});
+            }}>Profile</Nav.Link>
+            <Nav.Link onClick = {() => {
+              document.getElementById('skill').scrollIntoView({behavior: "smooth"});
+            }}>Skills</Nav.Link>
+            <Nav.Link onClick = {() => {
+              document.getElementById('project').scrollIntoView({behavior: "smooth"});
+            }}>Projects</Nav.Link>
+            <Nav.Link onClick = {() => {
+              document.getElementById('contact').scrollIntoView({behavior: "smooth"});
+            }}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Menu;
