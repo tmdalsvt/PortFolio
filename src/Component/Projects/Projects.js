c520108ced58ab95eef70a5e4bcb2e5bf5543151
tmdalsvt <54830688+tmdalsvt@users.noreply.github.com>
2019-12-11 20:14:25 +0900

    Add files via upload

---
+++ src/Component/Projects/Projects.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Projects.css';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className='Projects' id='project'>
+        <div className='Project_Title'>
+          <h1 className="Main_Title">Project</h1>
+        </div>
+        <div className="Project_Content">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;
