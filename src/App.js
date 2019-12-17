b8d7a3f1f260a607bff2859a1c4a40b989554a0a
tmdalsvt <54830688+tmdalsvt@users.noreply.github.com>
2019-12-18 01:16:18 +0900

    Modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Menu from './components/Menu/Menu';
 import Home from './components/Home/Home';
-import Profile from './components/Profile/Profile';
+import Career from './components/Career/Career';
 import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
@@ -12,7 +12,7 @@ class App extends Component {
       <div>
         {<Menu />}
         {<Home />}
-        {<Profile />}
+        {<Career />}
         {<Skills />}
         {<Projects />}
         {<Contact />}
